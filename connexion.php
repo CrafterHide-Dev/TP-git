b406f2ff9a9f881aeddbdc5a35968fa3ac34b6d1
CrafterHide <crafterhide.dev@outlook.com>
2025-05-15 08:55:48 +0200

    update

--- connexion.php
+++ connexion.php
@@ -4,8 +4,20 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title></title>
+		<!-- Fichiers CSS -->
+		<link rel="stylesheet" type="text/css" href="assets/css/general.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/footer.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/forms.css">
+		<!-- //////////// -->
 	</head>
 	<body>
+		<!-- Barre de navigation -->
+		<?php require_once('assets/php/navbar.php'); ?>
+		<!-- /////////////////// -->
 
+		<!-- Pied de page -->
+		<?php require_once('assets/php/footer.php'); ?>
+		<!-- //////////// -->
 	</body>
 </html>
